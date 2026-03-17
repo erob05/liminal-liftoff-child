536735be4e91d749ff328a56c219be6ddb945f08
erob05 <ericbenroberts@gmail.com>
2026-03-17 00:48:30 -0500

    Fixed layout issues
    Added several images and decorations

--- page-about.php
+++ page-about.php
@@ -8,7 +8,7 @@ get_header();
 <main id="primary" class="site-main">
 
     <!-- Making of the Game -->
-    <section class="about-row about-row--image-right">
+    <section class="about-row">
         <div class="about-text">
             <h2 class="about-title">Making of the Game</h2>
             <h3 class="about-subheading">Paragraph Header</h3>
@@ -43,7 +43,7 @@ get_header();
 
     <!-- Reflection / Image / Documentary -->
     <section class="about-row about-row--three">
-        <div class="about-text">
+        <div class="about-text reflection">
             <h2 class="about-title">Reflection</h2>
             <h3 class="about-subheading">Paragraph Header</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rhoncus auctor dictum. Mauris finibus ante sit amet elit vulputate, in pretium metus gravida. Duis porttitor gravida lacus, in mattis urna malesuada at. Sed at erat rhoncus, semper arcu nec, aliquet enim. Sed sit amet semper mi. Aenean lobortis auctor magna, eget gravida augue volutpat ut.</p>
--- page-meet-the-team.php
+++ page-meet-the-team.php
@@ -15,26 +15,23 @@ get_header();
     <section class="team-section team-section--cream">
         <h2 class="team-section-title">Project Leaders</h2>
         <div class="team-grid team-grid--3">
-            <?php for ($i = 0; $i < 3; $i++) : ?>
-            <div class="team-card">
-                <div class="team-card-image">
-                    <img src="https://placehold.co/300x250" alt="Team member" />
-                </div>
-                <div class="team-card-body">
-                    <div class="team-card-info">
-                        <div>
-                            <p class="team-card-name">John Doe</p>
-                            <p class="team-card-role">Lead Game Developer</p>
-                        </div>
-                        <div class="team-card-links">
-                            <a href="#" class="team-link team-link--b">B</a>
-                            <a href="#" class="team-link team-link--a">A</a>
-                        </div>
-                    </div>
-                    <p class="team-card-bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
-                </div>
+        <div>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/card.png" alt="Butler Community College" width="728" height="382" class="card"/>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/Eric_Roberts-3-cropped.jpg" alt="Butler Community College" width="150" height="150" class="profile-pic"/>
+            <div class="card-text">
+                <h3>Eric Roberts</h3>
+                <h4>Web Developer</h4>
+                <p>Lorem ipsum dolor sit amet <br>consectetur, adipisicing elit. <br> Similique beatae,</p>
             </div>
-            <?php endfor; ?>
+        </div>
+        <div>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/card.png" alt="Butler Community College" width="728" height="382" class="card"/>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/Eric_Roberts-3-cropped.jpg" alt="Butler Community College" width="150" height="150" class="profile-pic"/>
+        </div>
+        <div>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/card.png" alt="Butler Community College" width="728" height="382" class="card"/>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/Eric_Roberts-3-cropped.jpg" alt="Butler Community College" width="150" height="150" class="profile-pic"/>
+        </div>
         </div>
     </section>
 
