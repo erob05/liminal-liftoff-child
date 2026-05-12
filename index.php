d1b8b016d6770c4de3dbc7baf3d244dac7cc951a
erob05 <ericbenroberts@gmail.com>
2026-05-11 20:38:16 -0500

    Final
    Added links and polished layout

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ get_header();
 
     <section class="hero">
 		<div class="hero-inner">
-			<a href="#" class="hero-btn">Download Now</a>
+			<a href="https://drive.google.com/file/d/1cqXhUbVa5MVgo98079ezi7Ypa-UDh4gv/view" class="hero-btn">Download Now</a>
 		</div>
 
 		<div class="hero-bg" id="lottie-animation"></div>
